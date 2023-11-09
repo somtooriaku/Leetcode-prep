6188cd7523fb09acd68b744bbd0194f356843ed2
Somto Oriaku <tczar@Somtos-MacBook-Pro.local>
2023-11-09 10:50:58 -0500

    first commit

---
+++ helloworld.js
@@ -0,0 +1,17 @@
+// Stack array:
+
+class Stack {
+    constructor() {
+      this._array = [];
+    }
+  
+    push(newValue) {
+      this._array.push(newValue);
+    }
+  
+    pop() {
+      return this._array.pop();
+    }
+  }
+
+// Learning in JS
